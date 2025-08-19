472c6c15e23aad6ae40190660f8cdfdd0796400e
Vaibhav Kulkarni <vaibhav.kulkarni0359@gmail.com>
2025-08-19 15:24:24 +0530

    initial commit

---
+++ sunTime2/Profile.swift
@@ -0,0 +1,18 @@
+//
+//  Profile.swift
+//  sunTime2
+//
+//  Created by Vaibhav Kulkarni on 19/08/25.
+//
+
+import SwiftUI
+
+struct Profile: View {
+    var body: some View {
+        Text("Profile")
+    }
+}
+
+#Preview {
+    Profile()
+}
---
+++ sunTime2/Summary.swift
@@ -0,0 +1,18 @@
+//
+//  Summary.swift
+//  sunTime2
+//
+//  Created by Vaibhav Kulkarni on 19/08/25.
+//
+
+import SwiftUI
+
+struct Summary: View {
+    var body: some View {
+        Text("Summary")
+    }
+}
+
+#Preview {
+    Summary()
+}
